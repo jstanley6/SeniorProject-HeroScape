a99cf2356f605177d145de67b1db6547fa8b9ebc
Nathan <nmarsee@ewu.edu>
2025-03-21 23:02:39 -0700

    Update pdfGenerator template to include fields for all relevant info

--- HeroScape/Assets/Scripts/PdfGenerator.cs
+++ HeroScape/Assets/Scripts/PdfGenerator.cs
@@ -5,10 +5,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using QuestPDF.Infrastructure;
 using static TerrainEnumClass;
+using static TextUtilities;
+using UnityEngine.UI;
+using System.Text;
 
 public class PdfGenerator : MonoBehaviour
 {
     public List<TerrainPieces> scenarioTerrain;
+    public Text scenarioName;
+    public Text descriptionText;
+    public Text goalText;
+    public Text setupText;
+    public Text victoryText;
+    public Text rulesText;
 
     // Start is called before the first frame update
     void Start()
@@ -20,22 +29,47 @@ public class PdfGenerator : MonoBehaviour
             container.Page(page =>
             {
                 page.Size(PageSizes.A4);
-                page.Margin(2, Unit.Centimetre);
+                page.Margin(1.5f, Unit.Centimetre);
                 page.PageColor(Colors.White);
-                page.DefaultTextStyle(x => x.FontSize(20));
+                page.DefaultTextStyle(x => x.FontSize(12));
 
-                page.Header()
-                    .Text("Hello PDF!")
-                    .SemiBold().FontSize(36).FontColor(Colors.Blue.Medium);
+                /*page.Header()
+                    .Text(scenarioName.text)
+                    .SemiBold().FontSize(28).FontColor(Colors.Black);*/
 
                 page.Content()
                     .PaddingVertical(1, Unit.Centimetre)
                     .Column(x =>
                     {
-                        x.Spacing(20);
+                        x.Spacing(10);
 
-                        x.Item().Text(Placeholders.LoremIpsum());
-                        x.Item().Image(Placeholders.Image(200, 100));
+                        x.Item().Text(scenarioName.text)
+                        .SemiBold().FontSize(20).FontColor(Colors.Black);
+                        x.Item().Text(descriptionText.text).Italic();
+                        x.Item().Text(text =>
+                        {
+                            text.ParagraphSpacing(20);
+                            text.Span("GOAL: ").Bold();
+                            text.Span(goalText.text);
+                        });
+                        x.Item().Text(text =>
+                        {
+                            text.ParagraphSpacing(20);
+                            text.Span("SETUP: ").Bold();
+                            text.Span(setupText.text);
+                        });
+                        x.Item().Text(text =>
+                        {
+                            text.ParagraphSpacing(20);
+                            text.Span("VICTORY: ").Bold();
+                            text.Span(victoryText.text);
+                        });
+                        x.Item().Text(text =>
+                        {
+                            text.ParagraphSpacing(20);
+                            text.Span("SPECIAL RULES: ").Bold();
+                            text.Span(rulesText.text);
+                        });
                     });
 
                 page.Footer()
@@ -47,7 +81,7 @@ public class PdfGenerator : MonoBehaviour
                     });
             });
         })
-        .GeneratePdf("hello.pdf");
+        .GeneratePdf(TextUtilities.RemoveSpecialCharacters(scenarioName.text) + ".pdf");
     }
 }
 
